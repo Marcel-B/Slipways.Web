678b7677577a98291687c79b5733fd732287bd84
Marcel Benders <marcel.benders@cleverbridge.com>
2019-11-25 17:30:14 +0100

    (dev) Add form

---
+++ Slipways.Web/Data/Models/Slipway.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace com.b_velop.Slipways.Web.Data.Models
+{
+    public class Slipway
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Stadt / Ort")]
+        public string City { get; set; }
+
+        [Display(Name = "Längengrad")]
+        public double? Latitude { get; set; }
+
+        [Display(Name = "Breitengrad")]
+        public double? Longitude { get; set; }
+    }
+}
--- Slipways.Web/Pages/Index.cshtml.cs
+++ Slipways.Web/Pages/Index.cshtml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using com.b_velop.Slipways.Web.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -12,12 +9,22 @@ namespace Slipways.Web.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        public Slipway Slipway { get; set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger)
         {
             _logger = logger;
+            Slipway = new Slipway();
         }
 
         public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
         {
 
         }
