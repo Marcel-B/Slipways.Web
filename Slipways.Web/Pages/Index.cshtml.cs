99eb905243c55070e158422441a07c1fa269363f
Marcel Benders <marcel.benders@cleverbridge.com>
2019-12-09 12:43:19 +0100

    (dev) Add filter function

--- Slipways.Web/Pages/Index.cshtml.cs
+++ Slipways.Web/Pages/Index.cshtml.cs
@@ -7,10 +7,16 @@ using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace com.b_velop.Slipways.Web.Pages
 {
+    public class SlipwaysModel
+    {
+        public HashSet<Slipway> Slipways { get; set; }
+    }
+
     public class IndexModel : PageModel
     {
         private readonly IMemoryCache _cache;
@@ -18,8 +24,10 @@ namespace com.b_velop.Slipways.Web.Pages
         private readonly ISlipwayService _slipwayService;
         private readonly ILogger<IndexModel> _logger;
 
-        [BindProperty]
-        public HashSet<Slipway> Slipways { get; set; }
+        //[BindProperty]
+        //public HashSet<Slipway> Slipways { get; set; }
+
+        public SlipwaysModel Slipways { get; set; }
 
         public IndexModel(
             IMemoryCache cache,
@@ -35,12 +43,30 @@ namespace com.b_velop.Slipways.Web.Pages
 
         public async Task OnGetAsync()
         {
+            Slipways = new SlipwaysModel();
             if (!_cache.TryGetValue("Slipways", out HashSet<Slipway> slipways))
             {
                 slipways = new HashSet<Slipway>((await _slipwayService.GetSlipwaysAsync()));
                 _cache.Set("Slipways", slipways);
             }
-            Slipways = slipways;
+            Slipways.Slipways = slipways;
+        }
+
+        public IActionResult OnGetFilter(
+            [FromQuery] string search)
+        {
+            Slipways = new SlipwaysModel();
+            if (_cache.TryGetValue("Slipways", out HashSet<Slipway> slipways))
+            {
+                if (!string.IsNullOrWhiteSpace(search))
+                {
+                    Slipways.Slipways = new HashSet<Slipway>(slipways.Where(_ => _.Name.Contains(search) || _.City.Contains(search)).Distinct());
+                }
+                else
+                    Slipways.Slipways = slipways;
+            }
+            var partial = Partial("_SlipwayTable", Slipways);
+            return partial;
         }
 
         public void OnPost()
@@ -56,7 +82,7 @@ namespace com.b_velop.Slipways.Web.Pages
         public void Search(
             string search)
         {
-            if(_cache.TryGetValue("Slipways", out HashSet<Slipway> slipways))
+            if (_cache.TryGetValue("Slipways", out HashSet<Slipway> slipways))
             {
 
             }
