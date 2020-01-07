68df98109e49e7a00d00023a0d3a7fe720e846d9
Marcel Benders <marcel.benders@cleverbridge.com>
2020-01-07 18:14:44 +0100

    (dev) Work on CreatePorts

--- Slipways.Web/Areas/Admin/Pages/Port/Create.cshtml.cs
+++ Slipways.Web/Areas/Admin/Pages/Port/Create.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace com.b_velop.Slipways.Web.Areas.Admin.Pages.Port
@@ -40,14 +41,53 @@ namespace com.b_velop.Slipways.Web.Areas.Admin.Pages.Port
         public async Task OnGetAsync()
         {
             var waters = await _dataStore.Waters.GetValuesAsync();
-            Waters = new SelectList(waters, "Id", "Longname");
+            Waters = new SelectList(waters.OrderBy(_ => _.Longname), "Id", "Longname");
             var slipways = await _dataStore.Slipways.GetValuesAsync();
             Slipways = new SelectList(slipways, "Id", "Name");
         }
 
-        public void OnPostAsync()
+        public async Task<IActionResult> OnGetWaterBySlipwayIdAsync(
+            [FromQuery]Guid id)
         {
+            Console.WriteLine(id);
+            var slipways = await _dataStore.Slipways.GetValuesAsync();
+            var slipway = slipways.FirstOrDefault(_ => _.Id == id);
+            var water = slipway.Water;
+            var waters = await _dataStore.Waters.GetValuesAsync();
+            Waters = new SelectList(waters.OrderBy(_ => _.Longname), "Id", "Longname", id);
+            return new JsonResult(new { water.Id, water.Longname });
+        }
 
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var waters = await _dataStore.Waters.GetValuesAsync();
+            Waters = new SelectList(waters.OrderBy(_ => _.Longname), "Id", "Longname");
+            var slipways = await _dataStore.Slipways.GetValuesAsync();
+            Slipways = new SelectList(slipways, "Id", "Name");
+            if (!ModelState.IsValid)
+            {
+                Message = "Eingabefehler";
+                return Page();
+            }
+            try
+            {
+                var result = await _dataStore.Ports.AddAsync(Port);
+                if (result != null)
+                {
+                    Message = $"Marina '{Port.Name}' erfolgreich hinzugefügt";
+                    return RedirectToPage("./Index");
+                }
+                else
+                {
+                    Message = $"Fehler beim hinzufügen von '{Port.Name}'";
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(6666, $"Unexpected error occurred while inserting '{Port.Name}'", e);
+                Message = "Unerwarteter Fehler";
+            }
+            return Page();
         }
     }
 }
--- Slipways.Web/Areas/Admin/Pages/Port/Index.cshtml.cs
+++ Slipways.Web/Areas/Admin/Pages/Port/Index.cshtml.cs
@@ -2,21 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using com.b_velop.Slipways.Web.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace com.b_velop.Slipways.Web.Areas.Admin.Pages.Port
 {
     public class IndexModel : PageModel
     {
+        private IStoreWrapper _dataStore;
+        private ILogger<IndexModel> _logger;
+
         [TempData]
         public string Message { get; set; }
 
         [BindProperty]
         public HashSet<b_velop.Slipways.Data.Models.Port> Ports { get; set; }
 
-        public void OnGet()
+        public IndexModel(
+            IStoreWrapper dataStore,
+            ILogger<IndexModel> logger)
         {
+            _dataStore = dataStore;
+            _logger = logger;
+        }
+        public async Task OnGetAsync()
+        {
+            var ports = await _dataStore.Ports.GetValuesAsync();
+            Ports = ports ?? new HashSet<b_velop.Slipways.Data.Models.Port>();
         }
     }
 }
